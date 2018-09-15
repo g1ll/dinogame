17438bf7caa47f7c382d3a616e75e37ddbb9210c
gillvelleda@gmail.com <gillvelleda@gmail.com>
2018-09-15 14:49:55 -0300

    jQuery UI

--- public_html/js/dinogamedemo.js
+++ public_html/js/dinogamedemo.js
@@ -1,15 +1,28 @@
 /* 
  * Dino Game Demo 
  *  - Testando a matematica basica
- * Author: Gill Velleda Gonzales
+ * Authors: 
  * 
- * Version: 0.1
+ * IFSUL TEAM
+ *  • Gill Velleda Gonzales
+ *  • Daniel Trindade Cavalcanti
+ *  • Rafael Garcia Blanco
+ * 
+ * Version: 0.5
  * 
  * Use: jQuery and Konva
  */
 
 /* global Konva */
 $(document).ready(function () {
+    
+    
+    function dialogo(title,text){
+                $("#dialog").attr({'title': title});
+                $("#dialog").html(text);
+                $("#dialog").dialog();
+            }
+            
 
     var yodaImg = new Image();
     yodaImg.src = 'img/yoda.jpg';
@@ -21,16 +34,16 @@ $(document).ready(function () {
     yodaImg3.src = 'img/mega.jpg';
     var yodaImg4 = new Image();
     yodaImg4.src = 'img/para.jpg';
-    var yoda = createImage(50,50,100,100,yodaImg,true,0);
-    var lesto = createImage(50,50,100,100,yodaImg1,true,1);
-    var masto = createImage(50,50,100,100,yodaImg2,true,2);
-    var mega = createImage(50,50,100,100,yodaImg3,true,3);
-    var para = createImage(50,50,100,100,yodaImg4,true,4);
+    var yoda = createImage(50, 50, 100, 100, yodaImg, true, 0);
+    var lesto = createImage(50, 50, 100, 100, yodaImg1, true, 1);
+    var masto = createImage(50, 50, 100, 100, yodaImg2, true, 2);
+    var mega = createImage(50, 50, 100, 100, yodaImg3, true, 3);
+    var para = createImage(50, 50, 100, 100, yodaImg4, true, 4);
     var brmap = new Image();
     brmap.src = 'img/brmap.png';
-    var selecionados=[0,0,0,0,0] ;
+    var selecionados = [0, 0, 0, 0, 0];
     console.log(selecionados);
-    
+
     var stage = new Konva.Stage({
         container: 'container', // id of container <div>
         width: $(window).width(),
@@ -40,61 +53,74 @@ $(document).ready(function () {
     var layer = new Konva.Layer();
 
 // create our shape
-
-    
-    function createCircle(x,y,rad, fill, stroke,certo){
+    function createCircle(x, y, rad, fill, stroke, certo) {
         var circle = new Konva.Circle({
-        x: x,
-        y: y,
-        radius: rad,
-        fill: fill,
-        stroke: stroke,
-        strokeWidth: 4,
-        certo: certo
+            x: x,
+            y: y,
+            radius: rad,
+            fill: fill,
+            stroke: stroke,
+            strokeWidth: 4,
+            certo: certo,
+            visible: false
         });
         return circle;
     }
-    var noroeste = createCircle(stage.getWidth()/4, stage.getHeight()/3, stage.getWidth()/14, "green", "black",0);
-    var norte = createCircle(stage.getWidth()/2, stage.getHeight()/3.1, stage.getWidth()/14, "red", "black",1);
-    var nordeste = createCircle(stage.getWidth()/1.5, stage.getHeight()/2.3, stage.getWidth()/14, "yellow", "black",2);
-    var centroOeste = createCircle(stage.getWidth()/2.5, stage.getHeight()/1.8, stage.getWidth()/14, "green", "black",3);
-    var sudeste = createCircle(stage.getWidth()/1.7, stage.getHeight()/1.46, stage.getWidth()/14, "blue", "black",4);
-    var sul = createCircle(stage.getWidth()/2.15, stage.getHeight()/1.2, stage.getWidth()/14, "orange", "black",5);
-    
-    var button = createRect(50,600,100,50,"blue","black",4,false,1);
-    var rect = createRect(50,50,150,100,"blue","black",4,true,1);
+    var noroeste = createCircle(stage.getWidth() / 4, stage.getHeight() / 3, stage.getWidth() / 14, "green", "black", 0);
+    var norte = createCircle(stage.getWidth() / 2, stage.getHeight() / 3.1, stage.getWidth() / 14, "red", "black", 1);
+    var nordeste = createCircle(stage.getWidth() / 1.5, stage.getHeight() / 2.3, stage.getWidth() / 14, "yellow", "black", 2);
+    var centroOeste = createCircle(stage.getWidth() / 2.5, stage.getHeight() / 1.8, stage.getWidth() / 14, "green", "black", 3);
+    var sudeste = createCircle(stage.getWidth() / 1.7, stage.getHeight() / 1.46, stage.getWidth() / 14, "blue", "black", 4);
+    var sul = createCircle(stage.getWidth() / 2.15, stage.getHeight() / 1.2, stage.getWidth() / 14, "orange", "black", 5);
 
-    function createRect(x,y,w,h,f,s,sw,drag,id){
-    var rect = new Konva.Rect({
-        x: x,
-        y: y,
-        width: w,
-        height: h,
-        fill: f,
-        stroke: s,
-        strokeWidth: sw,
-        draggable: drag,
-       id: id });
-       return rect;
+    var button = createRect(
+            stage.getWidth() - 150,
+            stage.getHeight() - stage.getHeight() / 5, 100, 50, "green", "black", 4, false, 1);
+//    var rect = createRect(50,50,150,100,"blue","black",4,true,1);
+
+    var button_text = new Konva.Text({
+        x: button.attrs.x + 5,
+        y: button.attrs.y + 10,
+        text: 'Verificar',
+        fontSize: 25,
+        fontFamily: 'Calibri',
+        fill: 'white'
+    });
+
+
+    function createRect(x, y, w, h, f, s, sw, drag, id) {
+        var rect = new Konva.Rect({
+            x: x,
+            y: y,
+            width: w,
+            height: h,
+            fill: f,
+            stroke: s,
+            strokeWidth: sw,
+            draggable: drag,
+            cornerRadius: 20,
+            id: id});
+
+        return rect;
     }
 
-    function createImage(x,y,w,h,img,drag,id=null){
+    function createImage(x, y, w, h, img, drag, id = null) {
         var imagem = new Konva.Image({
-        x: 50,
-        y: 50,
-        width: 100,
-        height: 100,
-        image: img,
-        draggable: true,
-        id: id});
+            x: 50,
+            y: 50,
+            width: 100,
+            height: 100,
+            image: img,
+            draggable: true,
+            id: id});
         return imagem;
-}
+    }
 
     var mapa = new Konva.Image({
         x: 0,
         y: 70,
-        width: stage.getWidth()-70,
-        height: stage.getHeight()-70,
+        width: stage.getWidth() - 70,
+        height: stage.getHeight() - 70,
         image: brmap,
         draggable: false
     });
@@ -116,72 +142,164 @@ $(document).ready(function () {
 
     function isLoaded() {
         if (load === total) {
-            clearInterval(isLoaded);            
+            clearInterval(isLoaded);
             console.log('Pronto');
             init();
         } else {
             console.log('Carregando...');
         }
     }
-    
+
     yoda.on('dragend dragmove', function () {
-        selecionados[this.getAttr("id")]=testAllRegions(yoda);
+        if (this.attrs.width < sul.attrs.radius * 2)
+            selecionados[this.getAttr("id")] = testAllRegions(this);
+        else
+            selecionados[this.getAttr("id")] = testAllRegionsInnerCirc(this);
     });
-    
+
     lesto.on('dragend dragmove', function () {
-        selecionados[this.getAttr("id")]=testAllRegions(lesto);
+        if (this.attrs.width < sul.attrs.radius * 2)
+            selecionados[this.getAttr("id")] = testAllRegions(this);
+        else
+            selecionados[this.getAttr("id")] = testAllRegionsInnerCirc(this);
     });
-    
+
     masto.on('dragend dragmove', function () {
-        selecionados[this.getAttr("id")]=testAllRegions(masto);
+        if (this.attrs.width < sul.attrs.radius * 2)
+            selecionados[this.getAttr("id")] = testAllRegions(this);
+        else
+            selecionados[this.getAttr("id")] = testAllRegionsInnerCirc(this);
     });
-    
+
     mega.on('dragend dragmove', function () {
-        selecionados[this.getAttr("id")]=testAllRegions(mega);
+        if (this.attrs.width < sul.attrs.radius * 2)
+            selecionados[this.getAttr("id")] = testAllRegions(this);
+        else
+            selecionados[this.getAttr("id")] = testAllRegionsInnerCirc(this);
     });
-    
+
     para.on('dragend dragmove', function () {
-        selecionados[this.getAttr("id")]=testAllRegions(para);
+
+        if (this.attrs.width < sul.attrs.radius * 2)
+            selecionados[this.getAttr("id")] = testAllRegions(para);
+        else
+            selecionados[this.getAttr("id")] = testAllRegionsInnerCirc(para);
     });
-    
-    button.on('click',function () {
-        alert(selecionados);
+
+    button.on('click touchstart', function () {
+        console.log(selecionados);
+        mostraPontos();
     });
-    
-    function testAllRegions(ret){
-        retorno=0;
-        if(estadentro(ret, noroeste)==true){
-            retorno=1;
+    button_text.on('click touchstart', function () {
+        console.log(selecionados);
+        mostraPontos();
+    });
+
+    function mostraPontos() {
+        var p = 0;
+        var msg = "";
+        $.each(selecionados, function (i, v) {
+            p += v;
+        });
+        msg = "Pontos: " + p+"<br/>";
+
+        if (p === 5) {
+            msg += '\nParabéns!! Acertou Todos!!\nQuer reiniciar?';
+
+        } else if (p > 0) {
+            msg += '\nParabéns! Mas alguém está fora do lugar!';
+        } else {
+            msg += '\nContinue Tentando!';
         }
-        if(estadentro(ret, norte)==true){
-           retorno=1; 
+        if (dialogo("Resultado", msg) && p === 5) {
+            location.reload();
         }
-        if(estadentro(ret, nordeste)==true){
-            retorno=1;
+    }
+    ;
+
+    function testAllRegions(ret) {
+        retorno = 0;
+        if (estadentro(ret, noroeste)) {
+            retorno = 1;
         }
-        if(estadentro(ret, centroOeste)==true){
-           retorno=1; 
+        if (estadentro(ret, norte)) {
+            retorno = 1;
         }
-        if(estadentro(ret, sudeste)==true){
-           retorno=1; 
+        if (estadentro(ret, nordeste)) {
+            retorno = 1;
         }
-        if(estadentro(ret, sul)==true){
-           retorno=1; 
+        if (estadentro(ret, centroOeste)) {
+            retorno = 1;
+        }
+        if (estadentro(ret, sudeste)) {
+            retorno = 1;
+        }
+        if (estadentro(ret, sul)) {
+            retorno = 1;
         }
         return retorno;
     }
 
+    function testAllRegionsInnerCirc(ret) {
+        retorno = 0;
+
+        console.log(sudeste.attrs.x);
+        if (estadentro_circ(ret, noroeste)) {
+            retorno = 1;
+        }
+        if (estadentro_circ(ret, norte)) {
+            retorno = 1;
+        }
+        if (estadentro_circ(ret, nordeste)) {
+            retorno = 1;
+        }
+        if (estadentro_circ(ret, centroOeste)) {
+            retorno = 1;
+        }
+        if (estadentro_circ(ret, sudeste)) {
+            retorno = 1;
+        }
+        if (estadentro_circ(ret, sul)) {
+            retorno = 1;
+        }
+        return retorno;
+    }
+
+
+    function estadentro_circ(ret, circ) {
+        console.log(ret.attrs.x);
+        console.log(circ.attrs.x);
+        var rx1 = ret.attrs.x;
+        var ry1 = ret.attrs.y;
+        var rx2 = rx1 + ret.attrs.width;
+        var ry2 = ry1 + ret.attrs.height;
+        var cx = circ.attrs.x;
+        var cy = circ.attrs.y;
+        var cid = circ.attrs.certo;
+        var rid = ret.attrs.id;
+        console.log(cx + " >= " + rx1 + " &&" + cx + " <= " + rx2 + ") && (" + cy + ">=" + ry1 + " && " + cy + "<= " + ry2 + ") && " + cid + " === " + rid);
+        if ((cx >= rx1 && cx <= rx2) && (cy >= ry1 && cy <= ry2) && cid === rid) {
+
+            console.log('certo');
+
+            return true;
+        } else {
+            console.log('errado:' + circ.attrs.x + "  " + circ.attrs.certo);
+            return false;
+        }
+    }
 
     /**
      * @description Testa se um retângulo esta completamente dentro de uma circunferencia
      * @param retangulo = retangulo Konva.Rect();
      * @param circulo = circulo Konva.Circle();
      */
+    
     function estadentro(retangulo, circulo) {
         var n = 0;//contador de pontos dentro da circunferencia
-        if(stage.getWidth() <500){
-            var np=1;    
-        }else{
+        if (stage.getWidth() < 500) {
+            var np = 1;
+        } else {
             var np = 3;
         }//todos os pontos dentro do circulo
         var atr = retangulo.getAttrs();
@@ -208,22 +326,22 @@ $(document).ready(function () {
         console.log(retangulo.getAttr('id'));
         console.log(n);
         if (n >= np) {
-            if(c.certo==retangulo.getAttr('id')){
-            //se n > np o retangulo esta completamente dentro do circulo
+            if (c.certo == retangulo.getAttr('id')) {
+                //se n > np o retangulo esta completamente dentro do circulo
 //            console.log("Esta dentro do círculo!!");
 //            console.log(retangulo.position());
-            //alert("Está dentro do círculo!!");
-            retangulo.fill('red');
+                //alert("Está dentro do círculo!!");
+                retangulo.fill('red');
 //                console.log(retangulo);
-            retangulo.setStrokeWidth(10);
-            retangulo.stroke('black');
-            
-            console.log("circulo certo");
-            return 1;
-        }else{
-            console.log('circulo errado');
-            return 0;
-        }
+                retangulo.setStrokeWidth(10);
+                retangulo.stroke('black');
+
+                console.log("circulo certo");
+                return true;
+            } else {
+                console.log('circulo errado');
+                return false;
+            }
         } else {
             retangulo.fill('blue');
             retangulo.stroke('');
@@ -250,7 +368,8 @@ $(document).ready(function () {
     function init() {
         // add the shape to the layer
         layer.add(mapa);
-        
+//        layer.add(rect);
+
         layer.add(noroeste);
         layer.add(norte);
         layer.add(nordeste);
@@ -262,8 +381,8 @@ $(document).ready(function () {
         layer.add(masto);
         layer.add(mega);
         layer.add(para);
-        layer.add(rect);
         layer.add(button);
+        layer.add(button_text);
 
 // add the layer to the stage
         stage.add(layer);
